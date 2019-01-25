55282f1b58e84e640778a907ec49600bf91cfa1d
Cunha <r_cunha@tss-hr114-dm1.fc.ca>
2019-01-25 14:55:40 -0500

    updating v3

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//create jar, name jar, place string in jar
+//var firstName = "Renata";
+
+//alert is medthod(built in function in JS)
+//alert("Hello"  + firstName);
+
+//modern JS 
+//alert('Hello ${firstName}');
+
+//console.log("Hello" + "" + firstName);
+
+//console.log("javascript linked up");
